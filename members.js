6d9a33a201d007746dbcbf0305df26414026e7e0
davidbag50-hash <davidbag50@gmail.com>
2026-08-26 10:18:42 -0500

    Añade perfiles de integrantes de La Grey

---
+++ members.js
@@ -0,0 +1,10 @@
+window.LAGREY_MEMBERS=[
+ {id:'cristian',name:'Cristian',aliases:['cristian'],roles:['Todos los instrumentos','Voz'],instrument:'all',icon:'🎹🎸🎤'},
+ {id:'nicole',name:'Nicole',aliases:['nicole','nicol','nícol'],roles:['Piano','Voz'],instrument:'piano',icon:'🎹🎤'},
+ {id:'diego',name:'Diego',aliases:['diego'],roles:['Piano'],instrument:'piano',icon:'🎹'},
+ {id:'alberto',name:'Alberto',aliases:['alberto'],roles:['Guitarra','Voz'],instrument:'guitar',icon:'🎸🎤'},
+ {id:'nodier',name:'Nodier',aliases:['nodier'],roles:['Voz'],instrument:'voice',icon:'🎤'},
+ {id:'heather',name:'Heather',aliases:['heather'],roles:['Voz'],instrument:'voice',icon:'🎤'},
+ {id:'carol',name:'Carol',aliases:['carol'],roles:['Voz'],instrument:'voice',icon:'🎤'},
+ {id:'david',name:'David',aliases:['david'],roles:['Voz'],instrument:'voice',icon:'🎤'}
+];
