9ea9a628a9d954fc1404df2d409fd9149ab1ec6d
davidbag50-hash <davidbag50@gmail.com>
2026-08-26 15:41:42 -0500

    Actualiza caché para contraste forzado

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='la-grey-v2-12-contrast';
+const CACHE='la-grey-v2-13-contrast-fix';
 const ASSETS=['./','./index.html','./styles.css','./profiles.css','./performance.css','./stage-ui.css','./song-reader.css','./contrast.css','./songs.js','./chords.js','./members.js','./app.js','./profiles.js','./stage-ui.js','./song-reader.js','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
