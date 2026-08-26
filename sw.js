98c2e9cf74cb3392fa00fcb404862aae6c5da29e
davidbag50-hash <davidbag50@gmail.com>
2026-08-26 10:19:49 -0500

    Actualiza caché offline para perfiles

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='la-grey-v2-2';
-const ASSETS=['./','./index.html','./styles.css','./songs.js','./chords.js','./app.js','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png'];
+const CACHE='la-grey-v2-3-profiles';
+const ASSETS=['./','./index.html','./styles.css','./profiles.css','./songs.js','./chords.js','./members.js','./app.js','./profiles.js','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(cached=>cached||caches.match('./index.html'))))});
