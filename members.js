e6ad8019e10f6758bc98b58dd67ab9012590ff80
davidbag50-hash <davidbag50@gmail.com>
2026-08-28 10:12:32 -0500

    Eliminar himnos 311-530 sin acordes y volver a 1-310

--- members.js
+++ members.js
@@ -20,28 +20,6 @@ document.write('<script src="hymns-271-280.js"><\/script>');
 document.write('<script src="hymns-281-290.js"><\/script>');
 document.write('<script src="hymns-291-300.js"><\/script>');
 document.write('<script src="hymns-301-310.js"><\/script>');
-document.write('<script src="hymns-311-320.js"><\/script>');
-document.write('<script src="hymns-321-330.js"><\/script>');
-document.write('<script src="hymns-331-340.js"><\/script>');
-document.write('<script src="hymns-341-350.js"><\/script>');
-document.write('<script src="hymns-351-360.js"><\/script>');
-document.write('<script src="hymns-361-370.js"><\/script>');
-document.write('<script src="hymns-371-380.js"><\/script>');
-document.write('<script src="hymns-381-390.js"><\/script>');
-document.write('<script src="hymns-391-400.js"><\/script>');
-document.write('<script src="hymns-401-410.js"><\/script>');
-document.write('<script src="hymns-411-420.js"><\/script>');
-document.write('<script src="hymns-421-430.js"><\/script>');
-document.write('<script src="hymns-431-440.js"><\/script>');
-document.write('<script src="hymns-441-450.js"><\/script>');
-document.write('<script src="hymns-451-460.js"><\/script>');
-document.write('<script src="hymns-461-470.js"><\/script>');
-document.write('<script src="hymns-471-480.js"><\/script>');
-document.write('<script src="hymns-481-490.js"><\/script>');
-document.write('<script src="hymns-491-500.js"><\/script>');
-document.write('<script src="hymns-501-510.js"><\/script>');
-document.write('<script src="hymns-511-520.js"><\/script>');
-document.write('<script src="hymns-521-530.js"><\/script>');
 
 /* Voz + herramientas avanzadas. El diseño v35 se carga directamente desde index.html. */
 (()=>{const s=document.createElement('script');s.src='voice.js?v=35';s.onload=()=>{const p=document.createElement('script');p.src='voice-pro.js?v=35';p.defer=true;document.head.appendChild(p)};s.defer=true;document.head.appendChild(s)})();
