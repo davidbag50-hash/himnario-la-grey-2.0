cc1ac27832763f59e26e22f946135fe1258274ae
davidbag50-hash <davidbag50@gmail.com>
2026-08-28 16:50:44 -0500

    Load faithful selected La Grey home design last

--- settings-v1.js
+++ settings-v1.js
@@ -1,5 +1,5 @@
 (()=>{
- const css=document.createElement('link');css.rel='stylesheet';css.href='selected-design-v1.css?v=1';document.head.appendChild(css);
+ const css=document.createElement('link');css.rel='stylesheet';css.href='selected-home-v4.css?v=4';document.head.appendChild(css);
  const s=document.createElement('script');s.src='settings-v2.js?v=2';document.head.appendChild(s);
- const d=document.createElement('script');d.src='selected-design-v1.js?v=1';document.head.appendChild(d);
+ const d=document.createElement('script');d.src='selected-home-v4.js?v=4';document.head.appendChild(d);
 })();
