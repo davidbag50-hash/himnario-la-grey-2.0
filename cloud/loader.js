ac9e70b8b859734bb65d91b082c92cc3d4ae7800
davidbag50-hash <davidbag50@gmail.com>
2026-09-01 14:04:30 -0500

    Load ministry members roster UI

--- cloud/loader.js
+++ cloud/loader.js
@@ -12,6 +12,7 @@ function loadRoot(src){return new Promise((resolve,reject)=>{if(document.querySe
     for(const file of FILES)await load(file);
     await loadRoot('credential-isolation-v1.js');
     await loadRoot('account-onboarding-v2.js');
+    await loadRoot('members-roster-v1.js');
     document.documentElement.dataset.lagreyCloudReady='1';
     window.dispatchEvent(new CustomEvent('lagrey:cloud-ready',{detail:window.LAGREY_CLOUD?.getState?.()||null}));
   }catch(error){
